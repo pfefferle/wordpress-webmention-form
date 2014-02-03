d8c44d1576ab40fab90ef35fc69d97b5d306f263
pfefferle <pfefferle@gmail.com>
2014-02-03 17:06:12 +0100

    classified

--- webmention-form.php
+++ webmention-form.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name: WebMention Form
  Plugin URI: http://github.com/pfefferle/wordpress-webmention-form
- Description:
+ Description: Send WebMentions via a "comment" like form
  Author: Matthias Pfefferle
  Author URI: http://notizblog.org
  Version: 1.0.0-dev
@@ -10,19 +10,40 @@
  Text Domain: webmention_form
 */
 
-function webmention_form_init() {
-  load_plugin_textdomain( 'webmention_form', null, basename( dirname( __FILE__ ) ) );
-}
-add_action('init', 'webmention_form_init');
+if (!class_exists('WebMentionFormPlugin')) :
 
-function webmention_form() {
+// initialize plugin
+add_action('init', array('WebMentionFormPlugin', 'init'));
+
+/**
+ * WebMention Form Plugin Class
+ *
+ * @author Matthias Pfefferle
+ */
+class WebMentionFormPlugin {
+
+  /**
+   * initialize the plugin, registering WordPress hooks.
+   */
+  public static function init() {
+    add_action('comment_form_after', array('WebMentionFormPlugin', 'form'), 11);
+  }
+
+  /**
+   * render the form
+   */
+  public static function form() {
   ?>
-  <form id="webmention-form" action="<?php echo site_url('?webmention=endpoint'); ?>" method="post">
-    <label for="webmention-source"><?php _e('Responding with a post on your own blog? Send me a <a href="http://indiewebcamp.com/webmention">WebMention</a> <sup>(<a href="http://adactio.com/journal/6469/">?</a>)</sup>', 'webmention_form'); ?></label>
-    <input id="webmention-source" type="url" name="source" placeholder="URL/Permalink of your article" />
-    <input id="webmention-submit" type="submit" name="submit" value="Ping me!" />
-    <input id="webmention-target" type="hidden" name="target" value="<?php the_permalink(); ?>" />
-  </form>
+    <form id="webmention-form" action="<?php echo site_url('?webmention=endpoint'); ?>" method="post">
+      <p>
+        <label for="webmention-source"><?php _e('Responding with a post on your own blog? Send me a <a href="http://indiewebcamp.com/webmention">WebMention</a> <sup>(<a href="http://adactio.com/journal/6469/">?</a>)</sup>', 'webmention_form'); ?></label>
+        <input id="webmention-source" type="url" name="source" placeholder="URL/Permalink of your article" />
+        <input id="webmention-submit" type="submit" name="submit" value="Ping me!" />
+        <input id="webmention-target" type="hidden" name="target" value="<?php the_permalink(); ?>" />
+      </p>
+    </form>
   <?php
+  }
 }
-add_action('comment_form_after', 'webmention_form', 11);
+
+endif;
